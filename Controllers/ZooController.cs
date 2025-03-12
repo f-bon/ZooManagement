8ebfd080ddb492c7462e1f907d72dd7d06695512
Faith Martin <faith.martin@softwire.com>
2025-03-12 12:11:28 +0000

    fix DateTime issue and add Animal DTO

--- Controllers/ZooController.cs
+++ Controllers/ZooController.cs
@@ -1,5 +1,6 @@
 
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ZooManagement.Database;
@@ -9,20 +10,51 @@ namespace ZooManagement.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ZooController:ControllerBase{
+public class ZooController : ControllerBase
+{
 
     private readonly ILogger<ZooController> _logger;
 
     private ZooContext _zooContext;
-    public ZooController(ILogger<ZooController> logger, ZooContext zooContext){
+    public ZooController(ILogger<ZooController> logger, ZooContext zooContext)
+    {
         _logger = logger;
         _zooContext = zooContext;
 
     }
 
+
     [HttpGet("{id}")]
-    // [Route("{id}")]
-    public Animal? Get(int? id){
-        return _zooContext.Animal.Include(animal=>animal.Species).FirstOrDefault(a=>a.Id == id);   
+    public ActionResult Get(int? id)
+    {
+        try
+        {
+            var animal = _zooContext.Animal
+                .Include(animal => animal.Species)
+                .Select(animal => new
+                {
+                    animal.Id,
+                    animal.Name,
+                    DateOfBirth = DateTime.ParseExact(animal.DateOfBirth.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None),
+                    DateAcquired = DateTime.ParseExact(animal.DateAcquired.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None),
+                    animal.Sex,
+                    animal.Species.SpeciesName,
+                    animal.Species.Classification
+                })
+                .FirstOrDefault(animal => animal.Id == id);
+
+            if (animal is null)
+            {
+                return NotFound("No animal in system with this ID");
+            }
+            var animalDto = new AnimalDto(animal.Id, animal.Name, animal.DateOfBirth, animal.DateAcquired, animal.Sex, animal.SpeciesName, animal.Classification);
+            return Ok(animalDto);
+
+        }
+        catch (FormatException)
+        {
+            throw new FormatException("Invalid date format.");
+        }
     }
+
 }
---
+++ DTO/AnimalDto.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZooManagement.Models;
+
+public class AnimalDto
+{
+    public int Id { get; set; }    
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime DateAcquired { get; set; }
+    public string Sex { get; set; }
+    public string SpeciesName { get; set; }
+    public string Classification {get;set;}
+    
+    public AnimalDto()
+    {
+
+    }
+    public AnimalDto( int id, string name, DateTime dateOfBirth, DateTime dateAcquired, string sex, string speciesName, string classification)
+    {
+       Id = id;
+        Name = name;
+        DateOfBirth = dateOfBirth;
+        DateAcquired = dateAcquired;
+        Sex = sex;
+        SpeciesName = speciesName;
+        Classification = classification;
+    }
+}
